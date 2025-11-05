d1b87903e455dcc8458a6a714c8e7d84653c6117
Lorenzo Pioli <136918608+foughtmoss@users.noreply.github.com>
2025-11-05 11:03:11 +0100

    Remove duplicate contexts

--- update_question.py
+++ update_question.py
@@ -62,8 +62,7 @@ def get_context():
         "cattivo", "controverso", "vergognoso", "intimo", "sospetto", "codardo",
         "tradimento", "crush", "vergogna scolastica", "prima esperienza",
         "segreto mai confessato", "desiderio nascosto", "piacere proibito",
-        "paura più grande", "ossessione", "sesso", "crush nel gruppo", "sesso", "sesso",
-        "sesso", "sesso"
+        "paura più grande", "ossessione", "sesso", "crush nel gruppo", "sesso"
     ]
     return random.choice(contexts)
 
