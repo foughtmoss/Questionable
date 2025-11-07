0c4bca734b84fa3e5265a388f31e9ed4a985c2c5
Lorenzo Pioli <136918608+foughtmoss@users.noreply.github.com>
2025-11-07 17:38:52 +0100

    Update update_question.py

--- update_question.py
+++ update_question.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 
 load_dotenv()
 
-API_KEY = os.getenv("GEMINI_API_KEY2")
+API_KEY = os.getenv("GEMINI_API_KEY")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
